fdeb639def9cf250ed5a1e156ea57b51fa98537c
HusseinHany <hhany3997@gmail.com>
2023-08-09 01:47:23 +0300

    Display number value onClick in the console

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+let display = document.getElementById("display")
+let buttons = document.querySelectorAll("button")
+let displayValue = ""
+let firstValue = ""
+let secondValue = ""
+let operator = ""
+let result = ""
+
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        console.log(e.target.innerText);
+    })
+})

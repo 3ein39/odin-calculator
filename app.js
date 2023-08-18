7eb054d7f3b7a22628a7908a0813ce91f1053f08
HusseinHany <hhany3997@gmail.com>
2023-08-18 21:07:24 +0300

    Get it to work using key presses

--- app.js
+++ app.js
@@ -9,6 +9,52 @@ let result = ""
 let isSecondValue = false;
 
 
+addEventListener('keypress', (e) => {
+    // console.log(e.key);
+
+    if (e.key === "C") {
+        display.innerText = "0";
+        displayValue = "";
+        firstValue = "";
+        secondValue = "";
+        operator = "";
+        cnt = 0;
+        isSecondValue = false;
+        return;
+    }
+
+    if (e.key === "=" || e.key === "Enter") {
+        if (secondValue === "0" && operator === "/") {
+            alert("Division by zero is not allowed");
+            display.innerText = "";
+            displayValue = "";
+            firstValue = "";
+            secondValue = "";
+            operator = "";
+            cnt = 0;
+            isSecondValue = false;
+            return;
+        }
+        solve();
+        displayValue = result;
+        firstValue = result;
+        secondValue = "";
+        operator = "";
+        display.innerText = "";
+        updateDisplay();
+        return;
+    }
+    else if (e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/") {
+        operator = e.key;
+        isSecondValue = true;
+    } else if (!isSecondValue)
+        displayValue === "0" ? firstValue = e.key : firstValue += e.key;
+    else if (isSecondValue)
+        secondValue += e.key;
+
+    displayValue += e.key;
+    updateDisplay();
+})
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
         // console.log(e.target.innerText);
